79353b160c8b30e5e9c4b2c7adc10dcb67472ef7
Heitor-Saqueto <heitorsaqueto@hotmail.com>
2025-04-02 12:53:11 -0300

    finished

--- DesafioControleFluxo/src/main/java/org/yourcompany/yourproject/Contador.java
+++ DesafioControleFluxo/src/main/java/org/yourcompany/yourproject/Contador.java
@@ -4,13 +4,42 @@
 
 package org.yourcompany.yourproject;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
- * @author Heitor
+ * @author Heitor Saqueto
  */
 public class Contador {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = sc.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = sc.nextInt();
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException e) {
+			System.out.println("Error: "+e.getMessage());
+		}catch(InputMismatchException e){
+			System.out.println("Error: "+ e);
+		}
+		
+	}
+	public static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		if (parametroUm > parametroDois){
+			throw  new ParametrosInvalidosException("Invalide entry");
+		}else{
+
+			int contagem = parametroDois - parametroUm;
+			for (int i = 1; i <= contagem; i++){
+				System.out.println("Imprimindo o número "+ i);
+			}
+		}
+	}
 }
